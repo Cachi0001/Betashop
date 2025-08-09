18e993c3e555fc7e011d65b874ed45476d663bd9
Cachi0001 <passioncaleb5@gmail.com>
2025-08-09 20:13:24 +0100

    Final changes for backend deployment

--- backend/api/[...all].js
+++ backend/api/[...all].js
@@ -7,5 +7,10 @@ const app = require('../app');
 app.set('trust proxy', 1);
 
 module.exports = (req, res) => {
+  // Vercel mounts this function at /api/* and strips the "/api" prefix from req.url.
+  // Our Express app mounts routes at '/api/*', so we need to re-add the prefix here.
+  if (!req.url.startsWith('/api')) {
+    req.url = '/api' + (req.url.startsWith('/') ? req.url : `/${req.url}`);
+  }
   return app(req, res);
 };
